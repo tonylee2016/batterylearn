32edccf3a26bde84ef65becaeb77473ae910d449
Tony Li <tonylee2016@gmail.com>
2022-05-24 00:25:28 -0700

    move ahkab to req.txt

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,36 +3,36 @@ import numpy as np
 from pyfess.models import Flywheel
 from pyfess.utilities import ivp
 
-K = np.diag([5, 5, 5, 5, 5])  # add some
-C = np.zeros((5, 5))
-C[0, 1] = 100
-C[1, 0] = -100
-C[0, 0] = 1
-C[1, 1] = 1
-C[2, 2] = 1
-C[3, 3] = 1
-C[4, 4] = 1
-M = np.diag([10, 10, 5, 5, 5])
-fw = Flywheel(K, C, M, w0=50.0)
 
-v_t = np.arange(0, 10, 0.001)
-x0 = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0, 0, 0, 0, 0])
+def test_flywheel_base():
+    K = np.diag([5, 5, 5, 5, 5])  # add some
+    C = np.zeros((5, 5))
+    C[0, 1] = 100
+    C[1, 0] = -100
+    C[0, 0] = 1
+    C[1, 1] = 1
+    C[2, 2] = 1
+    C[3, 3] = 1
+    C[4, 4] = 1
+    M = np.diag([10, 10, 5, 5, 5])
+    fw = Flywheel(K, C, M, w0=50.0)
 
-sol = ivp(fw.ode, x0=x0, v_t=v_t)
+    v_t = np.arange(0, 10, 0.001)
+    x0 = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0, 0, 0, 0, 0])
 
-plt.plot(
-    sol.t,
-    np.transpose(
-        sol.y[
-            :2,
-        ]
-    ),
-)
-plt.legend(["thetax", "thetay", "x", "y", "z"])
-plt.show()
-print("done")
-plt.plot()
+    sol = ivp(fw.ode, x0=x0, v_t=v_t)
 
+    plt.plot(
+        sol.t,
+        np.transpose(
+            sol.y[
+                :2,
+            ]
+        ),
+    )
+    plt.legend(["thetax", "thetay", "x", "y", "z"])
+    plt.show()
+    plt.plot()
 
 def test_flywheel_model_with_bearing():
     pass
